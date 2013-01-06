5ac97404dd0c8868a41e80cfa9e54886b8f5d71d
Daniel Gimenes <daniel@coremax.com.br>
2013-01-06 17:00:30 -0300

    Fallback autoloader If it does not find its own autoloader, use the autoloader from the application that the module is coupled.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 
-chdir(__DIR__ . '/../../../');
-require 'vendor/autoload.php';
+/**
+ * If it does not find its own autoloader,
+ * use the autoloader from the application that the module is coupled.
+ */
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+    require __DIR__ . '/../../../vendor/autoload.php';
+}
